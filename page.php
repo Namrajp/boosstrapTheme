0efba3c8d6c8cf2e2b085f8f0cc08844d49c0d1b
Namraj Pudasaini <active99raj@gmail.com>
2020-01-03 08:27:27 +1100

    First commit Basic bootrap theme

---
+++ page.php
@@ -0,0 +1,14 @@
+ <?php get_header(); ?>
+ <div class="row">
+
+        <div class="col-sm-12 blog-main">
+		<?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
+
+			<?php get_template_part('content', get_post_format()) ; ?>
+
+<?php endwhile; endif; ?>
+        </div><!-- /.blog-main -->
+</div><!-- /.row -->
+
+ <?php get_sidebar(); ?>
+ <?php get_footer(); ?>
